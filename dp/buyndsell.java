60123c8c9e34a1cc911439c250cdd79edbe078d4
Shubham <shubhamsingh1840@gmail.com>
2020-10-03 12:23:16 +0530

    ./-

--- dp/buyndsell.java
+++ dp/buyndsell.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class buyndsell {
     public static void main(String[] args) {
 
@@ -60,6 +62,54 @@ public class buyndsell {
 
     }
 
+    // 2 stocks only leetcode 123
+    public int maxProfit_2(int[] prices) {
+        int ans = 0;
+        int i10 = 0;
+        int i11 = Integer.MIN_VALUE;
+        int i20 = 0;
+        int i21 = Integer.MIN_VALUE;
+        int prev1 = 0;
+        int prev2 = 0;
+        for (int ele : prices) {
+            // prev2=i20;
+            i20 = Math.max(i20, i21 + ele);
+            i21 = Math.max(i21, i10 - ele);
+            i10 = Math.max(i10, i11 + ele);
+            i11 = Math.max(i11, -ele);
+        }
+        return i20;
+    }
 
-    
+    // leetcode 188 k transactions...good question
+    public int maxProfit(int k, int[] prices) {
+        if (k > prices.length / 2)
+            return maxProfit_infi(prices);
+        int[] ik0 = new int[k + 1];
+        int[] ik1 = new int[k + 1];
+        Arrays.fill(ik0, 0);
+        Arrays.fill(ik1, Integer.MIN_VALUE);
+        for (int ele : prices) {
+            for (int ip = k; ip >= 1; ip--) {
+                ik0[ip] = Math.max(ik0[ip], ik1[ip] + ele);
+                if (k == 1)
+                    ik1[1] = Math.max(ik1[1], -ele);
+                else
+                    ik1[ip] = Math.max(ik1[ip], ik0[ip - 1] - ele);
+            }
+        }
+        return ik0[k];
+    }
+
+    public int maxProfit_infi(int[] prices) {
+        int ik0 = 0;
+        int ik1 = Integer.MIN_VALUE;
+        for (int val : prices) {
+            int temp = ik0;
+            ik0 = Math.max(ik0, ik1 + val);
+            ik1 = Math.max(ik1, temp - val);
+        }
+        return ik0;
+    }
+
 }
--- dp/questions.java
+++ dp/questions.java
@@ -78,8 +78,75 @@ public class questions {
 
     }
 
+    // minimum no of increasing subsequences
+    // we have to find longest increasing subsequence
 
-    //minimum no of increasing subsequences
-    //we have to find longest increasing subsequence
+    // box stacking problem
+
+    public static int maxHeight(int height[], int width[], int length[], int n) {
+        int[][] arr = new int[3 * n][3];
+        int ip = 0;
+        for (int i = 0; i < n; i++) {
+            int p1 = height[i];
+            int p2 = width[i];
+            int p3 = length[i];
+            if (p1 > p2) {
+                arr[ip][0] = p2;
+                arr[ip][1] = p1;
+                arr[ip][2] = p3;
+                ip++;
+            } else {
+                arr[ip][0] = p1;
+                arr[ip][1] = p2;
+                arr[ip][2] = p3;
+                ip++;
+            }
+            if (p2 > p3) {
+                arr[ip][0] = p3;
+                arr[ip][1] = p2;
+                arr[ip][2] = p1;
+                ip++;
+            } else {
+                arr[ip][0] = p2;
+                arr[ip][1] = p3;
+                arr[ip][2] = p1;
+                ip++;
+            }
+            if (p1 > p3) {
+
+                arr[ip][0] = p3;
+                arr[ip][1] = p1;
+                arr[ip][2] = p2;
+                ip++;
+            } else {
+                arr[ip][0] = p1;
+                arr[ip][1] = p3;
+                arr[ip][2] = p2;
+                ip++;
+            }
+        }
+
+        Arrays.sort(arr, (int[] a, int[] b) -> {
+            if (a[0] == b[0])
+                return b[1] - a[1];
+            return a[0] - b[0];
+
+        });
+        int[] dp = new int[arr.length];
+        int max = 0;
+        dp[0] = arr[0][2];
+        for (int i = 0; i < arr.length; i++) {
+            dp[i] = arr[i][2];
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j][1] < arr[i][1]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i][2]);
+                }
+                max = Math.max(dp[i], max);
+            }
+        }
+        return max;
+    }
+
+
     
 }
